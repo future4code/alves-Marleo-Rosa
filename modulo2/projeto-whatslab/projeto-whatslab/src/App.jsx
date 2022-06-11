1ad36ded1155250d72f7fbf0cf52618654028773
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-06-11 16:50:52 -0300

    Desenvolvimento do whatslab

---
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { BotaoEnviar, Quadradao } from "./style";
+class App extends React.Component {
+
+  state = {
+    inputUsuario: "",
+    inputMensagem: "",
+    mensagens: [
+      { usuario: "", mensagem: "" }
+    ]
+  }
+  adicionaUsuario = (event) => {
+    this.setState({ inputUsuario: event.target.value })
+  }
+  adicionaMensagem = (event) => {
+    this.setState({ inputMensagem: event.target.value })
+  }
+  adicionaMensagens = () => {
+    const novasMensagens = {
+      usuario: this.state.inputUsuario,
+      mensagem: this.state.inputMensagem
+    }
+    let copiaMensagem = [...this.state.mensagens, this.adicionaMensagens]
+    copiaMensagem.push(novasMensagens)
+    this.setState({ mensagens: copiaMensagem })
+
+    this.setState({ inputUsuario: "", inputMensagem: "" })
+  }
+
+  render() {
+    const declararMensagens = this.state.mensagens.map((item) => {
+      return (
+        <div>
+          <div> {item.usuario}</div>
+          <div>{item.mensagem}</div>
+        </div>
+      )
+    })
+    return (
+      <Quadradao>
+        <div>{declararMensagens}</div>
+        <div>
+          <input
+          value={this.state.inputUsuario}
+          onChange={this.adicionaUsuario}
+          placeholder="Usuário"
+          />
+          <input
+          value={this.state.inputMensagem}
+          onChange={this.adicionaMensagem}
+          placeholder="Texto da mensagem"
+          />
+          <BotaoEnviar onClick={this.adicionaMensagens}>Enviar</BotaoEnviar>
+        </div>
+      </Quadradao>
+    )
+  }
+}
+
+export default App;
---
+++ modulo2/projeto-whatslab/projeto-whatslab/src/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+export const BotaoEnviar = styled.button`
+font-family: monospace;
+padding: 5px 8px;
+&:hover{
+    background-color: black;
+    color: white;
+}
+`
+export const Quadradao = styled.div`
+
+`
