2a89c04c5aed452d9b8bdc885ff0f8bf0b89d837
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-08-10 23:07:51 -0300

    Aula introdução a TypeScript

---
+++ modulo4/intro-typescript/template-APIs-REST/src/ex2/exercicio2.js
@@ -0,0 +1,7 @@
+function imprimeTresCoresFavoritas() {
+    var cor1 = ("Azul");
+    var cor2 = ("Branco");
+    var cor3 = ("Preto");
+    console.log([cor1, cor2, cor3]);
+}
+(imprimeTresCoresFavoritas);
