1f08c02e42a5b13d4487895791a1b71693044918
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-08-12 20:45:17 -0300

    Ex 3

--- modulo4/lista-typescript/lista-typescript/src/ex3/exercicio3.ts
+++ modulo4/lista-typescript/lista-typescript/src/ex3/exercicio3.ts
@@ -1,1 +1,21 @@
-console.log("Ex3")
+enum GENERO {
+    ACAO = "ação",
+    DRAMA = "drama",
+    COMEDIA = "comédia",
+    ROMANCE = "romance",
+    TERROR = "terror"
+}
+
+type tipos = {
+    nome: string,
+    lancamento: string,
+    genero: string,
+    pontuacao?: string
+}
+
+function filmes(filme: tipos): object {
+    return filme
+}
+console.table(filmes({ nome: "Velozes e Furiosos", lancamento: "2001", genero: GENERO.ACAO }))
+console.table(filmes({ nome: "Até o último homem", lancamento: "2016", genero: GENERO.ACAO, pontuacao: "4.6" }))
+console.table(filmes({ nome: "Os Mercenários", lancamento: "2010", genero: GENERO.ACAO }))
