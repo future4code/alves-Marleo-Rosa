627a08900b25cba4141aa4d74403094b13924819
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-09-15 19:42:29 -0300

    Criação tabela Recipes

--- modulo5/projeto-backend-cookenu/Tabelas Cookenu.sql
+++ modulo5/projeto-backend-cookenu/Tabelas Cookenu.sql
@@ -9,3 +9,16 @@ CREATE TABLE users_cookenu (
 SELECT * FROM users_cookenu;
 
 DROP TABLE users_cookenu;
+
+CREATE TABLE recipes_cookenu (
+	id VARCHAR(255) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    create_Date VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users_cookenu(id)
+);
+
+SELECT * FROM recipes_cookenu;
+
+DROP TABLE recipes_cookenu;
