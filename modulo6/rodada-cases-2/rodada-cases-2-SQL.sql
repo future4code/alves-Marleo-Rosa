b1a5f7c09875792ae699cbbbc7ebe84e96724559
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-10-11 11:28:37 -0300

    rodada-cases-2

---
+++ modulo6/rodada-cases-2/rodada-cases-2-SQL.sql
@@ -0,0 +1,115 @@
+-- Entities
+-- -- Amb_Pizzas
+-- -- Amb_Ingredients
+-- -- Amb_Pizzas_Ingredients
+-- -- Amb_Order_Item
+
+DROP TABLE IF EXISTS Amb_Pizzas_Ingredients;
+DROP TABLE IF EXISTS Amb_Ingredients;
+DROP TABLE IF EXISTS Amb_Pizzas;
+DROP TABLE IF EXISTS Amb_Orders;
+DROP TABLE IF EXISTS Amb_Order_Items;
+
+CREATE TABLE IF NOT EXISTS Amb_Pizzas (
+	name VARCHAR(255) PRIMARY KEY,
+    price DECIMAL(4,2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Amb_Ingredients (
+	name VARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS Amb_Pizzas_Ingredients (
+	pizza_name VARCHAR(255) NOT NULL,
+    ingredient_name VARCHAR(255) NOT NULL,
+	FOREIGN KEY (pizza_name) REFERENCES Amb_Pizzas (name),
+    FOREIGN KEY (ingredient_name) REFERENCES Amb_Ingredients (name)
+);
+
+DROP TABLE IF EXISTS Amb_Orders_Item;
+
+CREATE TABLE IF NOT EXISTS Amb_Orders (
+	id VARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS Amb_Orders_Items (
+	id VARCHAR(255) PRIMARY KEY,
+    pizza_name VARCHAR(255) NOT NULL,
+    quantity TINYINT,
+	FOREIGN KEY (pizza_name) REFERENCES Amb_Pizzas (name)
+);
+
+CREATE TABLE IF NOT EXISTS Amb_Orders_Order_Items (
+	order_id VARCHAR(255) NOT NULL,
+    item_id VARCHAR(255) NOT NULL, 
+    FOREIGN KEY (order_id) REFERENCES Amb_Orders (id),
+	FOREIGN KEY (item_id) REFERENCES Amb_Orders_Items (id)
+);
+
+INSERT INTO Amb_Pizzas VALUES (
+	"Margherita",
+    5
+);
+
+INSERT INTO Amb_Pizzas VALUES (
+	"Bufala",
+    6
+);
+
+INSERT INTO Amb_Ingredients VALUES (
+      "mozzarella"
+);
+
+INSERT INTO Amb_Ingredients VALUES (
+      "mozzarella di bufala"
+);
+
+INSERT INTO Amb_Pizzas_Ingredients VALUES (
+	"Bufala",
+    "tomato"
+);
+
+INSERT INTO Amb_Pizzas_Ingredients VALUES (
+	"Bufala",
+    "mozzarella di bufala"
+);
+
+SELECT * FROM Amb_Pizzas;
+
+SELECT * FROM Amb_Pizzas
+JOIN Amb_Pizzas_Ingredients ON Amb_Pizzas_Ingredients.pizza_name = Amb_Pizzas.name
+WHERE name = "Margherita";
+
+SELECT * FROM Amb_Pizzas
+JOIN Amb_Pizzas_Ingredients ON Amb_Pizzas_Ingredients.pizza_name = Amb_Pizzas.name
+WHERE name = "Bufala";
+
+SELECT * FROM Amb_Pizzas;
+
+INSERT INTO Amb_Orders VALUES (
+	"p1"
+);
+
+RENAME TABLE Amb_Order_Item TO Amb_Order_Items;
+
+INSERT INTO Amb_Orders_Items VALUES (
+	"order-item-2",
+    "Bufala",
+    1
+);
+
+INSERT INTO Amb_Orders_Order_Items VALUES (
+	"p1",
+    "order-item-1"
+);
+
+INSERT INTO Amb_Orders_Order_Items VALUES (
+	"p1",
+    "order-item-2"
+);
+
+SELECT * FROM Amb_Orders
+JOIN Amb_Orders_Order_Items ON Amb_Orders.id = Amb_Orders_Order_Items.order_id
+JOIN Amb_Order_Items ON Amb_Order_Items.id = Amb_Orders_Order_Items.item_id
+JOIN Amb_Pizzas ON Amb_Order_Items.pizza_name = Amb_Pizzas.name
+WHERE Amb_Orders.id = "p1";
